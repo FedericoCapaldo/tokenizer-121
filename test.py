684fffd8adebf7f13e23a36c36e83ddede69fba4
Federico <fede6993@hotmail.it>
2017-01-22 23:32:26 -0800

    finished part A first two functions. second to be checked

--- test.py
+++ test.py
@@ -1,21 +1,58 @@
 import re
 import time
 
-start_time = time.time()
+# start_time = time.time()
 
-x = ""
-with open("file1.txt") as infile:
-    for line in infile:
-        x += line
+def tokeinze(my_path):
+    print("--- tokenizing... ---")
+    content = ""
+    with open(my_path) as infile:
+        for line in infile:
+            content += line
+    content = content.lower()
 
-r = re.compile(r'(?P<na>[^ \na-zA-Z0-9]*)(?P<a>[ \na-zA-Z0-9]+)')
-s = len(x)
-c = 0
-res = ""
-while c < s:
-    m = r.match(x, c)
-    res += m.group("a")
-    c += len(m.group("a")) + len(m.group("na"))
-# print res
+    r = re.compile(r'(?P<na>[^ \na-zA-Z0-9]*)(?P<a>[ \na-zA-Z0-9]+)')
+    s = len(content)
+    counter = 0
+    res = ""
+    while counter < s:
+        m = r.match(content, counter)
+        res += m.group("a")
+        counter += len(m.group("a")) + len(m.group("na"))
 
-print("---- %s seconds " % (time.time() - start_time))
+    return res.split()
+
+
+# print("---- %s seconds " % (time.time() - start_time))
+
+def computeWordFrequencies(list):
+    print("--- counting frequecy... ---")
+    list.sort()
+    counted = dict()
+
+    tempCounter = 1
+    previousWord = ""
+
+    for word in list:
+        if word == previousWord:
+            tempCounter += 1
+        else:
+            counted[word] = tempCounter
+            tempCounter = 1
+
+        previousWord = word
+
+    return counted
+
+
+
+# def computeWordFrequencies2(list):
+#     counted = dict()
+#
+#     for word in list:
+#         if counted.keys().contains()
+
+
+
+
+print(computeWordFrequencies(tokeinze("file1.txt")))
