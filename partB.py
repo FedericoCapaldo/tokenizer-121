ff88a6f1e8a74307938f2777e1fe8d1d493b59a4
Federico <fede6993@hotmail.it>
2017-01-25 18:08:49 -0800

    final retouch

--- partB.py
+++ partB.py
@@ -1,10 +1,10 @@
 import partA
 
-def findCommonWords(input1, input2): # runnint time O(n * n)
+def findCommonWords(type, input1, input2): # runnint time O(n * n)
     commonWordsCounter = 0
     listOfCommonWords = list()
-    input1 = partA.computeWordFrequencies(partA.tokeinze("data", input1))
-    input2 = partA.computeWordFrequencies(partA.tokeinze("data", input2))
+    input1 = partA.computeWordFrequencies(partA.tokeinze(type, input1))
+    input2 = partA.computeWordFrequencies(partA.tokeinze(type, input2))
 
     for word in input1:
         if word in input2:
@@ -16,4 +16,6 @@ def findCommonWords(input1, input2): # runnint time O(n * n)
 # TEST (expected match 6)
 # input1 = "We reviewed the trip and credited the cancellation fee. The driver has been notified"
 # input2 = "If a trip is cancelled more than 5 minutes after the driver-partner has confirmed the request, a cancellation fee will apply"
-# findCommonWords(input1, input2)
+# findCommonWords("data", input1, input2)
+# OR (expected match 30440)
+# findCommonWords("path", "file1.txt", "file1.txt")
