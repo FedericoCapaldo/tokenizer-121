b34fb6ca769a6fdfbb080bcf0126a226b77a8504
Federico <fede6993@hotmail.it>
2017-01-26 17:49:34 -0800

    corrected final error. Assignmenet completed. Room for improvement for larger files (4.0GB)

--- partB.py
+++ partB.py
@@ -17,7 +17,7 @@ def test_part_B(input1, input2):
     # (expected match 6)
     find_common_words("data", input1, input2)
     # OR (expected match 30440)
-    # findCommonWords("path", "file1.txt", "file1.txt")
+    # find_common_words("path", "file1.txt", "file1.txt")
 input1 = "We reviewed the trip and credited the cancellation fee. The driver has been notified"
 input2 = "If a trip is cancelled more than 5 minutes after the driver-partner has confirmed the request, a cancellation fee will apply"
 # test_part_B(input1, input2)
